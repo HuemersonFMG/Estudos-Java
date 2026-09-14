1320bee8de8c32a57630a87fe18c7bb6e09faa65
HuemersonFMG <huemersonsuper@gmail.com>
2026-09-13 23:43:39 -0300

    Adiciona projeto DIO-Estudo-Java

---
+++ DIO-Estudo-Java/EstudosEmDiante/src/Exemplo2For.java
@@ -0,0 +1,11 @@
+/**
+ * Exemplo2For
+ */
+public class Exemplo2For {
+
+  public static void main(String[] args) {
+    for(int contadorDePessoasRIR = 1; contadorDePessoasRIR <= 100000;contadorDePessoasRIR ++){
+      System.out.println("Quantas pessoas compraram o ingresso e devem ir por dia no Rock in Rio " + contadorDePessoasRIR + " pessoas ");
+    }
+  }
+}
